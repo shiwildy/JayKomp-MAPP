ec0410c5b5b43c249c40ea93f82511010c330981
shiwildy <shiwildy@gmail.com>
2024-12-05 10:14:38 +0700

    progress: dashboard

---
+++ app/app/src/main/java/com/wildysheverando/jaykomp/BerandaActivity.java
@@ -0,0 +1,17 @@
+package com.wildysheverando.jaykomp;
+
+import android.os.Bundle;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class BerandaActivity extends AppCompatActivity {
+
+}
